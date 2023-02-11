f666ca25f7c9b233e141775a49b191bb51d1ce80
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-02-11 17:21:50 +0200

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, useState} from 'react';
+import "./App.css"
 
-function App() {
-  return (
+const App = () => {
+    const [task, setTask] = useState<string>("");
+    const [deadline, setDeadline] = useState<number>(0);
+    const [todoList, setTodoList] = useState([]);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setTask(event.target.value)
+    };
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="header">
+            <div className="inputContainer">
+              <input type="text" placeholder="Task... "
+                     name="task"
+                     onChange={handleChange}/>
+              <button>Add Task</button>
+            </div>
+        </div>
+        <div className="todolist">
+
+        </div>
     </div>
   );
 }
